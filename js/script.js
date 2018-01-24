85a4d8633ae7dbf270f6f1bfce9a7db5833242ee
kevin lima <kevin.lima.code@gmail.com>
2018-01-24 23:21:32 +0000

    I made it work for a word

--- js/script.js
+++ js/script.js
@@ -6,30 +6,60 @@
 
 // Document Ready Function. All of your jQuery should go in here. 
 $(document).ready(function() {
-   $("button").click(function() {
-     var userInput = $("input").val();
-     console.log("user input is "+ userInput);
-     var pigLatin = pigLatinizer(userInput); 
-      console.log("piglatin is "+ pigLatin);
-      $("div").html(pigLatin);
-     
-   });
+    $("button").click(function() {
+        var userInput = $("input").val();
+        console.log("user input is " + userInput);
+        var pigLatin = pigLatinizersentence(userInput);
+        console.log("piglatin is " + pigLatin);
+        $("div").html(pigLatin);
+
+    });
 });
-  function pigLatinizer(word) {
-    var vowels =["a","e","i","o","u"];
+
+
+
+
+
+
+
+
+function pigLatinizersentence(userInput) {
+    var sentence = userInput.split(" ");
+    console.log("sentence =" + sentence);
+    
+    
+    
+    return pigLatinizer(userInput);
+}
+// for (i = 0; i < sentence.length; i++) {
+
+// }
+
+
+
+
+
+
+
+
+
+
+function pigLatinizer(word) {
+    var vowels = ["a", "e", "i", "o", "u"];
     var firstLetter = word[0];
     var indexOf = vowels.indexOf(firstLetter);
-    console.log("index of "+ indexOf);
-    var beginsWithVowel =  indexOf > -1;
-     if(beginsWithVowel){ 
-       return word + "yay";  
-     } else {  
-       return word + "ay";  
-     }            
+    console.log("index of " + indexOf);
+    var beginsWithVowel = indexOf > -1;
+    if (beginsWithVowel) {
+        return word + "yay";
+    }
+    else {
+        return word.slice(1) + firstLetter + "ay";
+    }
 }
 
 
-});
+
 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
@@ -40,7 +70,5 @@ $(document).ready(function() {
 
 
 // Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-
+//Loops through all the words in the sentence and transforms each word
+//It should return a transfromed sentance
